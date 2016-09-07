eaf970b87193119d8d84829d8d23e02a7cea0c55
Muhammad Awais Janjua <janjuain79@gmail.com>
2016-09-07 13:18:19 +1200

    First try on the Assignment 2
    Initial try on the assignment 2 of the R programming

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,44 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+  invMatrix <- NULL
+  
+  set <- function(y) {
+    x <<- y 
+    invMatrix <- NULL
+  }
+  
+  get <-  function() x
+  setInv <- function(inv) invMatrixx <<- inv
+  getInv <- function() invMatrix
+  list(set=set, get=get, setInv=setInv, getInv=getInv)
+  
+  
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  
+  invMatrix <- x$getInv()
+  
+  if(!is.null(invMatrix))
+  {
+    message("Getting Cached Data...")
+    return(invMatrix)
+  }
+  
+  data <- x$get()
+  invMatrix <- solve(data)
+  
+  x$setInv(invMatrix)
+  return(invMatrix)
+  
+  
 }
